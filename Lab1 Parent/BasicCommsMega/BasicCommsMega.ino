5c920ac1cd341fc12a2aecf839abe05236d2d207
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2024-01-08 18:50:41 -0700

    Add files for next part

---
+++ Lab1 Parent/BasicCommsMega/BasicCommsMega.ino
@@ -0,0 +1,20 @@
+void setup()  
+{
+  // Open serial communications with computer and wait for port to open:
+  Serial.begin(9600);
+
+  // Print a message to the computer through the USB
+  Serial.println("Hello Computer!");
+
+  // Open serial communications with the other Arduino board
+  Serial1.begin(9600);
+
+  // Send a message to the other Arduino board
+  Serial1.print("Hello other Arduino!");
+
+}
+
+void loop() // run over and over
+{
+
+}
