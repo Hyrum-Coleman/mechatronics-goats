576f6c701b79da371c9d8700d52c0a97a3f79de3
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2024-01-08 18:26:46 -0700

    some troll changes :troll:

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,14 +1,34 @@
 #include <Arduino.h>
-#define gamer_pin 13
+#include <SoftwareSerial.h>
+#include <ArduinoSTL.h>
+
+using namespace std;
+
+unsigned long time = 0;
+unsigned long timeOld = 0;
 
 int main() {
-  init();
-  pinMode(gamer_pin, OUTPUT);
-  digitalWrite(gamer_pin, HIGH);
+  init(); // Needed to get the board moving since we're not using setup() and loop()
+  Serial.begin(9600);
+
+  vector<int> nums = {1, 3, 5, 0, 12};
+  vector<int> odd_vals;
+  auto pred = [](int num){
+    auto ret = !(num % 2);
+    cout << "In function return val: " << ret << endl;
+    return ret;
+  };
+
+  remove_copy_if(nums.begin(), nums.end(), std::back_inserter(odd_vals), pred);
+
+  for (auto val : odd_vals) {
+    cout << val << " ";
+  }
+  cout << endl;
+
+  time = millis();
 
   while (true) {
     
-    delay(100);
-    digitalWrite(gamer_pin, !digitalRead(gamer_pin));
   }
 }
