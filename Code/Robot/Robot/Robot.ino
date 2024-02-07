f8897ba8fe6ff1e2d2ca619a168fd76b76c1027b
Hyrum <hyrum.coleman@gmail.com>
2024-02-06 21:16:05 -0700

    prelim wireless comm

--- Code/Robot/Robot/Robot.ino
+++ Code/Robot/Robot/Robot.ino
@@ -21,28 +21,56 @@ int main() {
 }
 
 void loop() {
-  bool start;
+  bool start_signal;
   states state = DRIVING;
+  String inputString = "";            // Holds incoming data from Serial3
+  boolean messageInProgress = false;  // Indicates if message parsing is in progress
   while (true) {
-    // Check if data is available to read from Serial3
-    if (Serial3.available() > 0) {  // zero will change here so I'm not going to try and predict what it will be
-      // Read the incoming byte from Serial3
-      char incomingByte = Serial3.read();
 
-      // Check if the incoming byte is '1' or '0' and set the start flag accordingly
-      if (incomingByte == '1') {
-        start = true;
-      } else if (incomingByte == '0') {
-        start = false;
-      }
-    }
+    String message = read_serial(inputString, messageInProgress);
+    
+    // Check if the incoming byte is '1' or '0' and set the start flag accordingly
+    if (message == "") {
 
-    if (start) {
-      switch (state) {
-        case DRIVING:
-          md.setSpeeds(180, 250, 500, 254);  //sets speeds of all 4 mecanum wheel motors
-          break;
-      }
+    } else if (message == "<1>") {
+      start_signal = true;
+    } else if (message == "<0>") {
+      start_signal = false;
     }
   }
+
+  if (start_signal) {
+    switch (state) {
+      case DRIVING:
+        md.setSpeeds(180, 250, 500, 254);  //sets speeds of all 4 mecanum wheel motors
+        break;
+    }
+  }
+}
+
+String read_serial(String inputString, bool messageInProgress) {
+  // Check if data is available to read from Serial3
+  while (Serial3.available() > 0) {  // read every byte!
+    // Read the incoming byte from Serial3
+    char inChar = Serial3.read();
+
+    // First character qualifier <
+    if (inChar == '<' && !messageInProgress) {
+      inputString = "";          // Clear the string
+      messageInProgress = true;  // Start collecting message
+      continue;                  // Skip further processing for this character
+    }
+    // Last character qualifier >
+    if (inChar == '>' && messageInProgress) {
+      // Process complete message
+      messageInProgress = false;  // Reset for next message
+      continue;                   // Skip further processing for this character
+    }
+
+    if (messageInProgress) {
+      inputString += inChar;
+    }
+  }
+
+  return inputString;
 }
