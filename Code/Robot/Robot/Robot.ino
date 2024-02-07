b0875223f86ff552de7124398d1f609f06a692cd
Hyrum <hyrum.coleman@gmail.com>
2024-02-06 20:40:42 -0700

    construct motor in global, set the speeds!

--- Code/Robot/Robot/Robot.ino
+++ Code/Robot/Robot/Robot.ino
@@ -1,19 +1,25 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include <ArduinoSTL.h>
+#include <DualTB9051FTGMotorShieldMod3230.h>
 
 using namespace std;
+
+DualTB9051FTGMotorShieldMod3230 md;
 
 int main() {
     init();  // Initialize board
     Serial.begin(9600);
+    Serial3.begin(9600);
 
 
+    md.enableDrivers();
     loop();
 }
 
 void loop() {
   while (true) {
 
+    
+    md.setSpeeds(180, 250, 500, 254); //sets speeds of all 4 mecanum wheel motors
   }
 }
