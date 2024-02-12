435ce8163bc6b37b0712666a94bb95490be4229d
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2024-02-12 11:00:36 -0700

    post lab5 trolling?

--- Lab5Parent/Coleman_Hyrum_Prelab5.m
+++ Lab5Parent/Coleman_Hyrum_Prelab5.m
@@ -1,8 +1,10 @@
+clc, clear, close all
+
 % Parameters
-R = 4.7e3;   % resistance in ohms
+R = 4.67e3;   % resistance in ohms
 C = 0.1e-6;  % capacitance in farads
 f_c = 1 / (2 * pi * R * C); % cutoff frequency
-f = logspace(log10(1), log10(1e6), 200); % Frequency range from 1 Hz to 1 MHz
+f = logspace(log10(10), log10(2e3), 200); % Frequency range from 10 Hz to 2 KHz
 
 % Call the function for low pass filter
 [magnitude_low_dB, phase_low_deg] = filter_response('low', R, C, f);
@@ -10,11 +12,21 @@ f = logspace(log10(1), log10(1e6), 200); % Frequency range from 1 Hz to 1 MHz
 % Call the function for high pass filter
 [magnitude_high_dB, phase_high_deg] = filter_response('high', R, C, f);
 
+lab_f = [10 20 40 150 340 500 700 1000 2000];
+
+low_pass_mag_data = [0.0333 0.0746 -0.0504 -0.7311 -2.6498 -4.3211 -6.1353 -8.2970 -12.4524];
+low_pass_phase_data = [-1.6230, -3.2310, -6.4150, -22.5700, -42.6100, -52.8600, -60.7200, -67.3000, -74.5100];
+
+
+high_pass_mag_data = [-20.0083, -17.8640, -15.8642, -7.5616, -3.1459, -1.8027, -1.0561, -0.5736, -0.1818];
+
+high_pass_phase_data = [78, 75.6000, 77.7200, 65.7500, 46.7200, 36.6800, 28.8800, 22.2800, 14.9600];
+
 % Plot the low pass filter response
-plot_filter_response(f, magnitude_low_dB, phase_low_deg, f_c, 'Low');
+plot_filter_response(f, magnitude_low_dB, phase_low_deg, f_c, 'Low', lab_f, low_pass_mag_data, low_pass_phase_data);
 
 % Plot the high pass filter response
-plot_filter_response(f, magnitude_high_dB, phase_high_deg, f_c, 'High');
+plot_filter_response(f, magnitude_high_dB, phase_high_deg, f_c, 'High', lab_f, high_pass_mag_data, high_pass_phase_data);
 
 
 function [magnitude_dB, phase_deg] = filter_response(filter_type, R, C, f)
@@ -42,30 +54,41 @@ function [magnitude_dB, phase_deg] = filter_response(filter_type, R, C, f)
     phase_deg = phase_rad .* (180/pi);
 end
 
-function plot_filter_response(f, magnitude_dB, phase_deg, f_c, filter_type)
+function plot_filter_response(f, magnitude_dB, phase_deg, f_c, filter_type, lab_f, lab_mag, lab_phase)
     % Create a new figure
     figure;
     
     % Plot the magnitude response
     subplot(2, 1, 1);
-    semilogx(f, magnitude_dB);
+    semilogx(f, magnitude_dB, 'DisplayName', 'Theoretical Response');
+    hold on;
+    plot(lab_f, lab_mag, 'o', 'Color', "#D95319", 'MarkerSize', 8, 'DisplayName', 'Lab Data');
+    hold off;
+
+    
     grid on;
     xlabel('Frequency (Hz)');
     ylabel('Magnitude (dB)');
     title([filter_type ' Pass Filter Magnitude Response']);
+    legend('Location', 'best');
     hold on;
-    plot([f_c f_c], ylim, 'r--'); % Cut-off frequency line
+    plot([f_c f_c], ylim, 'r--', 'DisplayName', 'Cutoff Frequency'); % Cut-off frequency line
     hold off;
     
     % Plot the phase response
     subplot(2, 1, 2);
-    semilogx(f, phase_deg);
+    semilogx(f, phase_deg, 'DisplayName', 'Theoretical Response');
+    hold on;
+    plot(lab_f, lab_phase, 'o', 'Color', "#D95319", 'MarkerSize', 8, 'DisplayName', 'Lab Data');
+    hold off;
+
     grid on;
     xlabel('Frequency (Hz)');
     ylabel('Phase (degrees)');
     title([filter_type ' Pass Filter Phase Response']);
+    legend('Location', 'best');
     hold on;
-    plot([f_c f_c], ylim, 'r--'); % Cut-off frequency line
+    plot([f_c f_c], ylim, 'r--', 'DisplayName', 'Cutoff Frequency'); % Cut-off frequency line
     hold off;
     
     % Add a main title to the figure
