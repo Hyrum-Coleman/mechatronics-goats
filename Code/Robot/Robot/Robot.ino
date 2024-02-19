976fa09612ec2a94f647e3d55d1f1d9c7bbad74e
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2024-02-19 11:59:28 -0700

    reverse if condition to reduce nesting

--- Code/Robot/Robot/Robot.ino
+++ Code/Robot/Robot/Robot.ino
@@ -61,54 +61,55 @@ void loop(JsonDocument doc) {
         }
         break;
       case DRIVING:
-        if (!sillyQueue->empty()) {
-          Move nextMove = sillyQueue->front();
-          sillyQueue->pop();
-
-          float motorSpeeds[4] = {0, 0, 0, 0}; // Initialize motor speeds
-
-          switch (nextMove.direction) {
-            case 1:  // forwards
-              wheelbase->computeWheelSpeeds(10, 0, 0, motorSpeeds);
-              break;
-            case 2:  // left
-              wheelbase->computeWheelSpeeds(0, -10, 0, motorSpeeds);
-              break;
-            case 3:  // backwards
-              wheelbase->computeWheelSpeeds(-10, 0, 0, motorSpeeds);
-              break;
-            case 4:  // right
-              wheelbase->computeWheelSpeeds(0, 10, 0, motorSpeeds);
-              break;
-            case 5:
-              smol_motors.setM1Speed(200);
-              delay(nextMove.time * 1000);
-              smol_motors.setM1Speed(0); // Stop motor after delay
-              continue;
-            case 6:
-              smol_motors.setM2Speed(200);
-              delay(nextMove.time * 1000);
-              smol_motors.setM2Speed(0); // Stop motor after delay
-              continue;
-            default:
-              Serial.println("Unexpected input in direction switch");
-              Serial.println(nextMove.direction);
-              break;
-          }
-
-          // For directional moves, map speeds and set mecanum motor speeds
-          if (nextMove.direction >= 1 && nextMove.direction <= 4) {
-            for (int i = 0; i < 4; i++) {
-              motorSpeeds[i] = map(motorSpeeds[i], -3.91, 3.91, -200, 200);
-              Serial.println(motorSpeeds[i]);
-            }
-            mecanum_motors.setSpeeds(motorSpeeds[0], motorSpeeds[1], motorSpeeds[2], motorSpeeds[3]);
-            delay(nextMove.time * 1000);
-            mecanum_motors.setSpeeds(0, 0, 0, 0); // Stop motors after delay
-          }
-        } else {
-          state = WAITING_TO_START; // Go back to waiting state if queue is empty
+        if (sillyQueue->empty()) {
+          state = WAITING_TO_START;  // Go back to waiting state if queue is empty
         }
+
+        Move nextMove = sillyQueue->front();
+        sillyQueue->pop();
+
+        float motorSpeeds[4] = { 0, 0, 0, 0 };  // Initialize motor speeds
+
+        switch (nextMove.direction) {
+          case 1:  // forwards
+            wheelbase->computeWheelSpeeds(10, 0, 0, motorSpeeds);
+            break;
+          case 2:  // left
+            wheelbase->computeWheelSpeeds(0, -10, 0, motorSpeeds);
+            break;
+          case 3:  // backwards
+            wheelbase->computeWheelSpeeds(-10, 0, 0, motorSpeeds);
+            break;
+          case 4:  // right
+            wheelbase->computeWheelSpeeds(0, 10, 0, motorSpeeds);
+            break;
+          case 5:
+            smol_motors.setM1Speed(200);
+            delay(nextMove.time * 1000);
+            smol_motors.setM1Speed(0);  // Stop motor after delay
+            continue;
+          case 6:
+            smol_motors.setM2Speed(200);
+            delay(nextMove.time * 1000);
+            smol_motors.setM2Speed(0);  // Stop motor after delay
+            continue;
+          default:
+            Serial.println("Unexpected input in direction switch");
+            Serial.println(nextMove.direction);
+            break;
+        }
+
+        // For directional moves, map speeds and set mecanum motor speeds
+        if (nextMove.direction >= 1 && nextMove.direction <= 4) {
+          for (int i = 0; i < 4; i++) {
+            motorSpeeds[i] = map(motorSpeeds[i], -3.91, 3.91, -200, 200);
+            Serial.println(motorSpeeds[i]);
+          }
+          mecanum_motors.setSpeeds(motorSpeeds[0], motorSpeeds[1], motorSpeeds[2], motorSpeeds[3]);
+          delay(nextMove.time * 1000);
+          mecanum_motors.setSpeeds(0, 0, 0, 0);  // Stop motors after delay
+        }
+
         break;
     }
   }
