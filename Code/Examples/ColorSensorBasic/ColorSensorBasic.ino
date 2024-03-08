341cce73f5eb1870daa736df2de3854729f6daaa
nawper02 <nawper02@gmail.com>
2024-03-08 14:42:34 -0700

    Get basic color sensor code working

--- Code/Examples/ColorSensorBasic/ColorSensorBasic.ino
+++ Code/Examples/ColorSensorBasic/ColorSensorBasic.ino
@@ -1,42 +1,41 @@
-#include <Adafruit_APDS9960.h>
-
+#include "Adafruit_APDS9960.h"
 Adafruit_APDS9960 apds;
 
 void setup() {
   Serial.begin(9600);
 
-  apds.enable();
-
-  if (!apds.begin()) {
-    Serial.println("Initialization Failed :("); // might need the Adafruit_BusIO library idk tho. it's in big L Libraries
+  if(!apds.begin()){
+    Serial.println("Initialization Failed :(");
   }
+  else Serial.println("Device initialized!");
 
-  apds.enableGesture(false);
-  apds.enableColor();
-  // put your setup code here, to run once:
-
+  //enable color sensign mode
+  apds.enableColor(true);
 }
 
 void loop() {
-  static uint16_t r;
-  static uint16_t g;
-  static uint16_t b;
-  static uint16_t c;
-
-  if (!apds.colorDataReady()) {
-    return;
+  //create some variables to store the color data in
+  uint16_t r, g, b, c;
+  
+  //wait for color data to be ready
+  while(!apds.colorDataReady()){
+    delay(5);
   }
 
-  Serial.println("Color Detected!");
-
-  apds.getColorData(r, g, b, c);
-
-  Serial.print("RGB: (");
+  //get the data and print the different channels
+  apds.getColorData(&r, &g, &b, &c);
+  Serial.print("red: ");
   Serial.print(r);
-  Serial.print(", ");
+  
+  Serial.print(" green: ");
   Serial.print(g);
-  Serial.print(", ");
+  
+  Serial.print(" blue: ");
   Serial.print(b);
-  Serial.print(") Clear Channel: ");
+  
+  Serial.print(" clear: ");
   Serial.println(c);
+  Serial.println();
+  
+  delay(100);
 }
